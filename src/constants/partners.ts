6dc19bdef8b36e4eb8930ee5082d9931b7b8abac
MACoding4U <mohammed711adel@gmail.com>
2025-04-22 14:40:15 +0200

    Partners folder moved to public/

--- src/constants/partners.ts
+++ src/constants/partners.ts
@@ -1,22 +1,22 @@
 export const PartnersData = [
   {
     title: "VERSACE",
-    logo: "/landing/partners/partner-1.svg",
+    logo: "/partners/partner-1.svg",
   },
   {
     title: "ZARA",
-    logo: "/landing/partners/partner-2.svg",
+    logo: "/partners/partner-2.svg",
   },
   {
     title: "GUCCI",
-    logo: "/landing/partners/partner-3.svg",
+    logo: "/partners/partner-3.svg",
   },
   {
     title: "PRADA",
-    logo: "/landing/partners/partner-4.svg",
+    logo: "/partners/partner-4.svg",
   },
   {
     title: "Calvin Klein",
-    logo: "/landing/partners/partner-5.svg",
+    logo: "/partners/partner-5.svg",
   },
 ]
