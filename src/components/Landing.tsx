23ae718babdffeba7cc571ce667fbec619462521
MACoding4U <mohammed711adel@gmail.com>
2025-04-23 12:51:33 +0200

    Some changes in landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -5,9 +5,9 @@ export default function Landing() {
   return (
     <section className="landing p-14 bg-[#F2F0F1] pb-0 max-md:p-0">
       <div className="container mx-auto flex items-center justify-between max-lg:flex-col max-lg:justify-center">
-        <div className="left-section grid gap-8 w-1/2 max-lg:w-full pb-8 max-md:px-8 max-md:py-8">
-          <h1 className="title text-[54px] font-bold leading-[1] whitespace-nowrap max-md:text-[38px]">FIND CLOTHES <br /> THAT MATCHES <br /> YOUR STYLE</h1>
-          <p className="desc text-[15px] leading-[1.6] text-slate-500 font-light font-sans">Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+        <div className="left-section grid gap-8 w-1/2 max-lg:w-full pb-8 max-md:px-4 max-md:py-8">
+          <h1 className="title text-[54px] font-bold leading-[1] whitespace-nowrap max-md:text-[36px] max-md:text-center">FIND CLOTHES <br /> THAT MATCHES <br /> YOUR STYLE</h1>
+          <p className="desc text-[15px] leading-[1.6] text-slate-500 font-light font-sans max-md:text-center">Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
           <button className="shop-now transition-all border-2 border-solid border-black hover:bg-transparent hover:text-black hover:font-semibold py-4 px-14 max-md:w-full bg-black rounded-full text-[18px] font-[300] text-white w-fit cursor-pointer font-sans">Shop Now</button>
           <div className="branding flex items-center gap-12 font-sans text-center max-md:justify-center max-md:flex-wrap">
             { landingData.map((ele, index) => (
